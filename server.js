7b831b91da2cc53c68e612632f19786f9bfd5485
romeukjr <romeukjr@hotmail.com>
2018-06-24 23:08:18 -0300

    Set up BrowserBook database API

--- server.js
+++ server.js
@@ -28,101 +28,10 @@ db.once('open', function() {
   console.log("DB connection alive");
 });
 
-// Teste models lives here
-var Teste     = require('./app/models/teste');
-
 // ROUTES FOR OUR API
 // =============================================================================
+require('./app/routes')(app); // configure our routes
 
-// create our router
-var router = express.Router();
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-	// do logging
-    console.log('Something is happening.');
-	next();
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
-});
-
-// on routes that end in /testes
-// ----------------------------------------------------
-router.route('/testes')
-
-	// create a teste (accessed at POST http://localhost:8080/api/testes)
-	.post(function(req, res) {
-		var teste = new Teste();		// create a new instance of the Teste model
-		teste.name = req.query.name;  // set the testes name (comes from the request)
-		teste.save(function(err) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Teste created!' });
-		});
-
-		
-	})
-
-	// get all the testes (accessed at GET http://localhost:8080/api/testes)
-	.get(function(req, res) {
-		Teste.find(function(err, testes) {
-			if (err)
-				res.send(err);
-
-			res.json(testes);
-		});
-	});
-
-// on routes that end in /testes/:teste_id
-// ----------------------------------------------------
-router.route('/testes/:teste_id')
-
-	// get the teste with that id
-	.get(function(req, res) {
-		Teste.findById(req.params.teste_id, function(err, teste) {
-			if (err)
-				res.send(err);
-			res.json(teste);
-		});
-	})
-
-	// update the teste with this id
-	.put(function(req, res) {
-		Teste.findById(req.params.teste_id, function(err, teste) {
-
-			if (err)
-				res.send(err);
-
-			teste.name = req.query.name;
-			teste.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Teste updated!' });
-			});
-
-		});
-	})
-
-	// delete the teste with this id
-	.delete(function(req, res) {
-		Teste.remove({
-			_id: req.params.teste_id
-		}, function(err, teste) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted' });
-		});
-	});
-
-
-// REGISTER OUR ROUTES -------------------------------
-app.use('/api', router);
 
 // START THE SERVER
 // =============================================================================
